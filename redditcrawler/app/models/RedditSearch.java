cc0ce61afa6fb5f12511ec3d0c82c4d76623728d
Yugansh Goyal <34838617+yugansh77@users.noreply.github.com>
2021-11-06 17:51:15 -0400

    Model skeleton created

--- redditcrawler/app/models/RedditSearch.java
+++ redditcrawler/app/models/RedditSearch.java
@@ -12,6 +12,5 @@ public class RedditSearch {
 		this.searchKey = searchKey;
 	}
 	
-	//https://www.youtube.com/watch?v=QMLkACi0Obc&list=PLYPFxrXyK0Bx9SBkNhJr1e2-NlIq4E7ED&index=12&ab_channel=RadixCode
-	//go over this for remaining
+	
 }
